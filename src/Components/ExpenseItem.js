8780bdb0a221f973501518367576db252b2de7c1
Mayukh <mayukhchatterjee2023@gmail.com>
2023-03-30 19:45:32 +0530

    Adding Multiple Expenses

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,14 +1,21 @@
-function ExpenseItem(){
-    return (
-    <div><h2>Expense Item</h2>
-      <h4>Food Rs 10</h4>
-      <h4>Petrol Rs 100</h4>
-      <h4>Movies Rs 200</h4>
-      
-    </div>
-       
-    )
-}
+import "./ExpenseItem.css";
 
+function ExpenseItem(props) {
+  const expenseDate = new Date(2023, 2, 28);
+  const expenseTitle = "Food";
+  const expenseAmount = 300;
+  const locationOfExpenditure='Office';
+  
+  return (
+    <div className="expense-item">
+      <div>{props.date.toLocaleDateString()}</div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <h2>{props.location}</h2>
+        <div className="expense-item__price">{props.amount}$</div>
+      </div>
+    </div>
+  );
+}
 
 export default ExpenseItem;
