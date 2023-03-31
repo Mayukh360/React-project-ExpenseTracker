e6c73b805cab2e7ebeff5db2ab2a1e6e1121ccf4
Mayukh <mayukhchatterjee2023@gmail.com>
2023-03-31 10:36:59 +0530

    Adding components and styling

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,14 +1,12 @@
+import "./ExpenseDate.js";
+import ExpenseDate from "./ExpenseDate.js";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
-  const expenseDate = new Date(2023, 2, 28);
-  const expenseTitle = "Food";
-  const expenseAmount = 300;
-  const locationOfExpenditure='Office';
-  
+
   return (
     <div className="expense-item">
-      <div>{props.date.toLocaleDateString()}</div>
+     <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <h2>{props.location}</h2>
