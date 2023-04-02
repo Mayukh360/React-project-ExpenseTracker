bfccc86ed4a32b0574048eb06ebe57d4eb535c42
Mayukh <mayukhchatterjee2023@gmail.com>
2023-04-02 09:52:24 +0530

    Adding Delete Expense without Stying

--- src/Components/Expenses/ExpenseItem.js
+++ src/Components/Expenses/ExpenseItem.js
@@ -1,18 +1,29 @@
+import React, { useState } from "react";
 import ExpenseDetails from "./ExpenseDetails.js";
 import ExpenseDate from "./ExpenseDate.js";
 import "./ExpenseItem.css";
 import "./Expenses.css";
+import card from "./card.js"
 
 
 const ExpenseItem=(props)=> {
+  const [isDeleted, setIsDeleted] = useState(false);
 
+  const deleteExpense = () => {
+    setIsDeleted(true);
+  };
+
+  if (isDeleted) {
+    return null;
+  }
   return (
-    <div className="expense-item expenses">
+    <card className="expense-item expenses">
      <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
       <ExpenseDetails title={props.title} location={props.location} amount={props.amount}/>
       </div>
-    </div>
+      <button onClick={deleteExpense}>Delete Expense</button>
+    </card>
   );
 }
 
---
+++ src/Components/Expenses/card.js
@@ -0,0 +1,9 @@
+import "./card.css"
+
+function card(props){
+
+    return (
+        <div className="card">{props.children}</div>
+    );
+}
+export default card;
